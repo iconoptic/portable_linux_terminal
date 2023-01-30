0e747557020369bccb7f7722f5242259733b0c76
iconoptic <btgrant@gmail.com>
2023-01-30 19:43:37 +0000

    Fixed memory overflow

--- keyboard-firmware/keeb_firmware.c
+++ keyboard-firmware/keeb_firmware.c
@@ -11,14 +11,17 @@
 
 const uint LED_PIN = PICO_DEFAULT_LED_PIN;
 
-const int led[LEDNUM] = {
+
+//define array of LED pins
+const short led[LEDNUM] = {
 	27,	//LED_CLK
 	7,	//LED_R
 	6,	//LED_G
 	28	//LED_B
 };
 
-const int sw_out[SW_L] = {
+//define pins used transmit (pico -> switch array)
+const short sw_out[SW_L] = {
 	8,	//Y0
 	9,	//Y1
 	10,	//Y2
@@ -29,7 +32,8 @@ const int sw_out[SW_L] = {
 	20	//Y7
 };
 
-const int sw_in[SW_L] = {
+//define pins used to receive (switch array -> pico)
+const short sw_in[SW_L] = {
 	12,	//X0
 	13,	//X1
 	14,	//X2
@@ -45,13 +49,13 @@ void init_pins() {
 	gpio_set_dir(LED_PIN, GPIO_OUT);
 
 	//Init LED pins
-	for (int i = 0; i < LEDNUM; i++) {
+	for (short i = 0; i < LEDNUM; i++) {
 		gpio_init(led[i]);
 		gpio_set_dir(led[i], GPIO_OUT);
 	}
 
 	//Init switch pins
-	for (int i = 0; i < SW_L; i++) {
+	for (short i = 0; i < SW_L; i++) {
 		gpio_init(sw_out[i]);
 		gpio_init(sw_in[i]);
 		gpio_set_dir(sw_out[i], GPIO_OUT);
@@ -59,14 +63,13 @@ void init_pins() {
 	}
 }
 
-int *poll_sw() {
-	int *coords = malloc(SW_L * 2 * sizeof(int));
-	int pIndex = 0;
-	for (int i = 0; i < SW_L; i++) {
+short *poll_sw(short *coords) {
+	short pIndex = 0;
+	for (short i = 0; i < SW_L; i++) {
 		gpio_put(sw_out[i], 1);
-		for (int j = 0; j < SW_L; j++) {
+		for (short j = 0; j < SW_L; j++) {
 			if ( gpio_get(sw_in[j]) ) {
-				coords[pIndex]= j;	//X
+				coords[pIndex]= j;		//X
 				coords[pIndex+SW_L] = i;	//Y
 				pIndex++;
 			}
@@ -79,27 +82,23 @@ int *poll_sw() {
 void main(void) {
 	stdio_init_all();
 	init_pins();
-	int *coords;
+	//declare array & allocate space
+	short *coords = malloc(SW_L * 2 * sizeof(short));
 	while (true) {
-//		struct keeb_coord aPin = poll_sw();
-		coords = poll_sw();
-	//	printf("%d,%d\n", coords[0], coords[1]);
+		//remove contents of array
+		for (short i = 0; i < SW_L*2; i++) coords[i] = -1;
+		coords = poll_sw(coords);
 		for (int i = 0; i < SW_L; i++) {
-			printf("X%d: %d\n", i, coords[i]);
-			printf("Y%d: %d\n\n", i, coords[i+SW_L]);
+			if ( coords[i] != -1) {
+				printf("X%d: %d\n", i, coords[i]);
+				printf("Y%d: %d\n\n", i, coords[i+SW_L]);
+			}
 		}
-		/*for (int i = 0; i < aPin.x[0]; i++) {
-			gpio_put(led[0], 1);
-			sleep_ms(100);
-			gpio_put(led[0], 0);
-			sleep_ms(100);
-		}*/
 		gpio_put(LED_PIN, 1);
 		//for (int i = 0; i < LEDNUM; i++) gpio_put(led[i], 1);
 		sleep_ms(250);
 		//for (int i = 0; i < LEDNUM; i++) gpio_put(led[i], 0);
 		gpio_put(LED_PIN, 0);
 		sleep_ms(250);
-		free(coords);
 	}
 }
