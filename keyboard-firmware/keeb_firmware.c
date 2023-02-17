c26c42ded366165e55cbf908aacf37a9549fe99a
iconoptic <btgrant@gmail.com>
2023-02-17 22:47:04 +0000

    Updated PWM code

--- keyboard-firmware/keeb_firmware.c
+++ keyboard-firmware/keeb_firmware.c
@@ -11,15 +11,17 @@
 #define SW_L 8
 
 // PWM
-const int pwm_wrap = 65465;
-const float pwm_duty[3] = {0.25, 0.5, 0.75};
-
+const float pwm_freq = 10.0;
+const int pwm_T_us = (int)((1.0 / pwm_freq) * 1000000.0); 
+static int pwm_wrap;
+static int pwm_level[4];
 
 // LED
 const char LED_PIN = PICO_DEFAULT_LED_PIN;
 
 //define led clock pin
 const char led_clk = 27;
+static char clk_sl, clk_ch;
 
 //define array of LED pins
 const char led[LEDNUM] = {
@@ -54,24 +56,8 @@ const char sw_in[SW_L] = {
 	17,	//X6
 	16	//X7
 };
-	
+
 void init_pins() {
-	gpio_init(LED_PIN);
-	gpio_set_dir(LED_PIN, GPIO_OUT);
-
-	//Init LED pins
-	gpio_init(led_clk);
-	gpio_set_dir(led_clk, GPIO_OUT);
-	for (char i = 0; i < LEDNUM; i++) {
-		//gpio_init(led[i]);
-		//gpio_set_dir(led[i], GPIO_OUT);
-		//set pwm
-		gpio_set_function(led[i], GPIO_FUNC_PWM);
-		led_sl[i] = pwm_gpio_to_slice_num(led[i]);
-		led_ch[i] = pwm_gpio_to_channel(led[i]);
-		printf("Slice=%d\tChannel=%d\n", led_sl[i], led_ch[i]);
-	}
-
 	//Init switch pins
 	for (char i = 0; i < SW_L; i++) {
 		gpio_init(sw_out[i]);
@@ -79,6 +65,36 @@ void init_pins() {
 		gpio_set_dir(sw_out[i], GPIO_OUT);
 		gpio_set_dir(sw_in[i], GPIO_IN);
 		gpio_put(sw_out[i], 1);
+	}
+}
+
+void configPWM () {
+	//calculate divder and wrap
+	float clkFreq = 125000000.0;
+	double divider = (clkFreq / (4096.0 * pwm_freq))/16.0;
+	pwm_wrap = (int)(clkFreq / divider / pwm_freq - 1.0);
+	float duty = 0.0;
+
+	//Calculate level for each duty
+	for (char i = 0; i < sizeof pwm_level / sizeof pwm_level[0]; i++) {
+		pwm_level[i] = (int)((float)pwm_wrap*duty);
+		duty += 0.25;
+	}
+
+	//clk
+	gpio_set_function(led_clk, GPIO_FUNC_PWM);
+	clk_sl = pwm_gpio_to_slice_num(led_clk);
+	clk_ch = pwm_gpio_to_channel(led_clk);
+
+	//tx LEDs
+	for (char i = 0; i < LEDNUM; i++) {
+		gpio_set_function(led[i], GPIO_FUNC_PWM);
+		led_sl[i] = pwm_gpio_to_slice_num(led[i]);
+		led_ch[i] = pwm_gpio_to_channel(led[i]);
+
+		//set pwm clock divider and wrap value
+		pwm_set_clkdiv(led_sl[i], divider);
+		pwm_set_wrap(led_sl[i], pwm_wrap);
 	}
 }
 
@@ -98,16 +114,13 @@ char *poll_sw(char *pinArr) {
 }
 
 void writeLED (char ledIndex, char writeVal) {
-	pwm_set_clkdiv_int_frac (led_sl[ledIndex], 38, 3);
-	pwm_set_wrap (led_sl[ledIndex], pwm_wrap);
-	pwm_set_chan_level(led_sl[ledIndex], led_ch[ledIndex], (int)((float)pwm_wrap*pwm_duty[writeVal-1]));
+	pwm_set_chan_level(led_sl[ledIndex], led_ch[ledIndex], pwm_level[writeVal]);
+	printf("%d\n", pwm_level[writeVal]);
 }
 
-void blinkClk () {
-	gpio_put(led_clk, 1);
-	sleep_ms(200);
-	gpio_put(led_clk, 0);
-	sleep_ms(200);
+void blinkClk() {
+	pwm_set_chan_level(clk_sl, clk_ch, pwm_level[2]);
+	add_alarm_in_us(pwm_T_us, 
 }
 
 void writePWM (char writeVal) {
@@ -127,28 +140,32 @@ void writePWM (char writeVal) {
 	blinkClk();
 }
 
+
 void main(void) {
 	stdio_init_all();
 	init_pins();
+	configPWM();
 	//declare array & allocate space
 	char *pinArr = malloc(SW_L * 2 * sizeof(char));
-	/*while (true) { 
-		writeLED (0, 1);
-		writeLED (1, 2);
-		writeLED (2, 3);
-	}*/
-	writePWM(0x1b);
-	while (true) {
+
+	printf("%d\n", pwm_T_us);
+
+	for (char i = 0; i < LEDNUM; i++) {
+		writeLED(i, i+1);
+		pwm_set_enabled(led_sl[i], true);
+	}
+	//writePWM(0x1b);
+	/*while (true) {
 		//remove contents of array
 		for (char i = 0; pinArr[i] != 255; i++) pinArr[i] = 255;
 		pinArr = poll_sw(pinArr);
 		for (char i = 0; pinArr[i] != 255; i++) {
 			//if ( pinArr[i] != 255) {
 			printf("%d: %d\n", i, pinArr[i]);
-			writePWM(pinArr[i]);
+			//writePWM(pinArr[i]);
 			//}
 		}
 		//for (unsigned char i = 0; i < LEDNUM; i++) pwm_set_enabled(led_sl[0], false);
-	}
+	}*/
 }
 
